2e6d4afe8f7014222f011b195890b1793df4dfda
Evanicio Silva <evanicio29@gmail.com>
2023-03-02 13:17:33 -0300

    NOVOS

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-//alert("Seja Bem Vindo(A)")
+/*var idade = 40;
+if(idade < 18 ){
+     console.log("Ainda uma criança");
+}
+else if(idade >= 20 && idade < 45){
+     console.log("Esta virano um adulto");
+}
+else if(idade >= 60){
+     console.log("Virou Um Idoso");
+}
+#########################################################
+var idade = 60;
+
+if(idade < 10){
+     console.log("Eu Sou uma criança");
+}
+else if(idade <= 60 && idade > 45){
+     console.log("Eu sou Um Adulto");
+}
+else if(idade > 80 ){
+     console.log("Eu Sou Um Vei");
+}
+
+#########################################################
+
+
+let x = 10;
+let y = 5;
+console.log(x > y)
+
+
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+let a = 10;
+let b = 5;
+console.log( a != b);
+
+let preco = 40.3;
+if(preco <= 45){
+console.log("preço 45.00 Esta barata")
+}
+else{
+     console.log("ESTA MUITO CARO")
+}
+#########################################################
+*/
+// CONDICIONAL TERNARIO
+/*
+let insMember = true;
+let shipping = insMember ? 2 : 10;
+console.log("Voce e membro" : "voce nao e membro");
+console.log("frete:" + shipping); */
+/*
+let age = 14;
+let Isadult = ((age >= 18 && ag < 60) ? "SIM" : "NAO");
+console.log(Isadult);
+*/
