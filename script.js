63073d5bdb5884ba8e941423aa71447440b36513
Evanicio Silva <evanicio29@gmail.com>
2023-03-06 15:20:43 -0300

    ESTUDANDO SEM PARAR

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
-/*var idade = 40;
-if(idade < 18 ){
-     console.log("Ainda uma criança");
+/*let profission = 'bombeiro'
+
+console.log('PROFISSÃO '  + profission);
+
+switch (profission) {
+     case'fiscal':
+     console.log('Sua camisa sera VERDE');
+      
+     break;
+
+     case'bombeiro':
+
+     console.log('Sua camisa sera Vermelha');
+
+     break;
+
+     case 'policial':
+
+     console.log('Sua Camisa sera Azul')
+
+     break;
+
+     default:
+
+     console.log('Sua camisa sera Preta');
+
+     break
 }
-else if(idade >= 20 && idade < 45){
-     console.log("Esta virano um adulto");
-}
-else if(idade >= 60){
-     console.log("Virou Um Idoso");
-}
-#########################################################
-var idade = 60;
-
-if(idade < 10){
-     console.log("Eu Sou uma criança");
-}
-else if(idade <= 60 && idade > 45){
-     console.log("Eu sou Um Adulto");
-}
-else if(idade > 80 ){
-     console.log("Eu Sou Um Vei");
-}
-
-#########################################################
-
-
-let x = 10;
-let y = 5;
-console.log(x > y)
-
-
-
-let w = "10";
-let z = 10;
-console.log(w === z);
-
-let a = 10;
-let b = 5;
-console.log( a != b);
-
-let preco = 40.3;
-if(preco <= 45){
-console.log("preço 45.00 Esta barata")
-}
-else{
-     console.log("ESTA MUITO CARO")
-}
-#########################################################
 */
-// CONDICIONAL TERNARIO
+
+
 /*
-let insMember = true;
-let shipping = insMember ? 2 : 10;
-console.log("Voce e membro" : "voce nao e membro");
-console.log("frete:" + shipping); */
-/*
-let age = 14;
-let Isadult = ((age >= 18 && ag < 60) ? "SIM" : "NAO");
-console.log(Isadult);
+function MaiorDeIdade(idade){
+     if (idade >= 18){
+          return  true ;
+     } else{
+          return false;
+     }
+}
+
+let idade = 18;
+let verificacao = MaiorDeIdade(idade);
+
+if(verificacao){
+     console.log('È MAIOR de  idade');
+} else{
+     console.log('È MENOR de idade');
+}
+
 */
+
+//REFAZENDO
+function maiorDeIdade(idade){
+     if(idade >= 18){
+          return true ;
+     }
+     else{
+          return false;
+     }
+}
+
+let idade = 18;
+let verificacao = maiorDeIdade(idade);
+
+if(verificacao){
+     console.log('È MAIOR de idade');
+} else{
+     console.log('È MENOR de idade')
+}
